1d7bad7397f7b0de95724aa0a605ec6368e1c121
Ria Rivera <65005610+ria-rivera@users.noreply.github.com>
2024-08-20 21:00:03 +0800

    Modify config file

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://riarivera.com',
+  url: '/portfolio/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ria-rivera', // Usually your GitHub org/user name.
-  projectName: 'website', // Usually your repo name.
+  projectName: 'portfolio', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
