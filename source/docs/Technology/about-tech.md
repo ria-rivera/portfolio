16eecadb3c2b05c0ff89016dec547ef694d9bef3
Ria Rivera <65005610+ria-rivera@users.noreply.github.com>
2024-08-20 20:47:34 +0800

    Add first half of files

---
+++ source/docs/Technology/about-tech.md
@@ -0,0 +1,3 @@
+# Technology
+
+I have done the full gamut of technical writing: from writing articles for a tech magazine, to doing technical marketing, and eventually focusing on technical documentation and UX writing.
---
+++ source/docusaurus.config.js
@@ -0,0 +1,102 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Ria Rivera',
+  tagline: 'Creative Writer | Technical Writer',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://riarivera.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ria-rivera', // Usually your GitHub org/user name.
+  projectName: 'website', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Ria Rivera',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'aboutSidebar',
+            position: 'left',
+            label: 'About',
+          },
+          {to: '/docs/Essays/about-essays', label: 'Essays', position: 'left'},
+          {to: '/docs/Food and Travel/about-food-travel', label: 'Food and Travel', position: 'left'},
+          {to: '/docs/Technology/about-tech', label: 'Technology', position: 'left'},
+          {
+            href: 'https://github.com/ria-rivera',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Ria Rivera. Built with Docusaurus. Vector art by <a href="https://www.freepik.com/author/pikisuperstar">pikisuperstar</a>.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
