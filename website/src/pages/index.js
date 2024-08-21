994c69fdd0244bd2c5c8009fb1d39daf27648d54
Ria Rivera <65005610+ria-rivera@users.noreply.github.com>
2024-08-21 12:23:58 +0800

    Modify page title

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Creative and Technical Writer`}
+      description="Portfolio website">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
