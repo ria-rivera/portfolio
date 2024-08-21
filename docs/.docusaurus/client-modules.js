d6a084a3ffee6dec5b3e1f098cf2a97c197523ad
Ria Rivera <65005610+ria-rivera@users.noreply.github.com>
2024-08-21 09:17:45 +0800

    Troubleshooting

--- docs/.docusaurus/client-modules.js
+++ docs/.docusaurus/client-modules.js
@@ -1,6 +1,3 @@
 export default [
-  require("/Users/riarivera/Documents/work/port4/portfolio/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/riarivera/Documents/work/port4/portfolio/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/riarivera/Documents/work/port4/portfolio/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/riarivera/Documents/work/port4/portfolio/docs/src/css/custom.css"),
+
 ];
